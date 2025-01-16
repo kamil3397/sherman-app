6771ed89027568aa6475f2dae4eeca261e4c1ff1
kamil3397 <kkgolkiper33@gmail.com>
2025-01-16 18:36:33 +0100

    drobne poprawki po merge'u

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,28 @@
 import './App.css';
-import { lazy, Suspense } from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { MainLayout } from 'layouts';
 import { ToastContainer } from 'react-toastify';
-import Calendar from 'pages/Training/Calendar';
 import { Loader } from 'components/Loader';
-//przerob import na lazy import
 
 const HomePage = lazy(() => import('pages/Home/HomePage'));
 const Register = lazy(() => import('pages/Auth/Register'));
 const Login = lazy(() => import('pages/Auth/Login'));
 const Calendar = lazy(() => import('pages/Training/Calendar'));
 
-
 function App() {
   return (
     <>
       <Router>
         <Suspense fallback={<Loader/>}>
-        <Routes>
-          <Route path="/" element={<MainLayout />} > {/*to tez do lazy? */}
-            <Route index element={<HomePage />} />
-            <Route path="/register" element={<Register />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/training' element={<Calendar />} />
-          </Route>
-        </Routes>
+          <Routes>
+            <Route path="/" element={<MainLayout />} >
+              <Route index element={<HomePage />} />
+              <Route path="/register" element={<Register />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/training' element={<Calendar />} />
+            </Route>
+          </Routes>
         </Suspense>
       </Router>
       <ToastContainer/>
--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,37 +4,35 @@ import { Box } from '@mui/material';
 import Logo from 'images/loader.svg';
 
 export const Loader = () => {
-    return (
-        <Box
-            sx={{
-                position: 'fixed',
-                top: 0,
-                left: 0,
-                width: '100%',
-                height: '100%',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                zIndex: 1
-            }}
-        >
-            <CircularProgress
-                size={100}
-                sx={{
-                    color: '#444b51', 
-                    position: 'absolute',
-                }}
-            />
-            {/*JAK zrobic zeby spinner był w około logo zeby zdąrzyło się załadować??? */}
-            
-            <img
-                src={Logo as unknown as string}
-                alt="Sherman Shooting Team Logo"
-                style={{
-                    width: '80px',
-                    height: '80px',
-                }}
-            />
-        </Box>
-    );
+  return (
+    <Box
+      sx={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 1
+      }}
+    >
+      <CircularProgress
+        size={100}
+        sx={{
+          color: '#444b51',
+          position: 'absolute',
+        }}
+      />
+      <img
+        src={Logo as unknown as string}
+        alt="Sherman Shooting Team Logo"
+        style={{
+          width: '80px',
+          height: '80px',
+        }}
+      />
+    </Box>
+  );
 };
